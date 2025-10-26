e32421fac1736642c1810bf287fcbb2d3ff19f96
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2025-10-26 18:31:04 +0530

    add new code

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+// ScrollReveal animations
+ScrollReveal().reveal('.meet-mateo, .latest-news, .catch-pokemon, .items-for-adventure, .team-go-rocket, .get-up-and-go, footer', {
+    delay: 200,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    reset: true
+});
